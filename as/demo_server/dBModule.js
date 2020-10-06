fe3a9ad9a0dd11062915b5543c3fdbab08aaac9c
Osidur <hugo.pollack@elev.ga.ntig.se>
2020-10-06 10:16:08 +0200

    ff

--- as/demo_server/dBModule.js
+++ as/demo_server/dBModule.js
@@ -6,23 +6,4 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // we're connected!
 });
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  age: Number
-});
-
-const Person = mongoose.model('Person', personSchema);
-
-exports.storePerson = (name, email, age) => {
-    var person = new Person({
-        name: name, 
-        email: email, 
-        age: age 
-       })
-   
-     person.save(()=>{
-       console.log("Successfully saved person in database!")
-     })
-}
+//hjelp mej snela? :)?
