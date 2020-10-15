ff8e6d222c525c3d17de0f6e435c91673c545544
Osidur <hugo.pollack@elev.ga.ntig.se>
2020-10-15 14:46:18 +0200

    fffas

--- as/demo_server/PersonModel.js
+++ as/demo_server/PersonModel.js
@@ -1,23 +1,20 @@
 const mongoose = require('mongoose');
-
 const personSchema = new mongoose.Schema({
     name: String,
     email: String,
     age: Number
 });
-  
+
 const Person = mongoose.model('Person', personSchema);
-  
+
 exports.createPerson = function(name, email, age) {
     var person = new Person({
         name: name, 
         email: email, 
         age: age 
     })
-
     return person;
 }
-
 exports.getAllPeople = async () => {
     let people = await Person.find({})
     return people
--- as/demo_server/app.js
+++ as/demo_server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dBModule = require('./dBModule')
-const personModel = require('./PersonModel');
+const personModel = require('./PersonModel')
+const messageModel = require('./MessageModel')
 const app = express()
 const port = 3000
 const clientDir = __dirname + "\\client\\"
@@ -8,22 +9,24 @@ const nameList = ['a', 'b', 'c']
 
 app.use(express.static(clientDir))
 
-app.set('viewengine', 'ejs');
+app.set('viewengine', 'ejs')
+
+app.get('/', (request, response) => {
+  response.render("pages/index.ejs", {nameList: nameList})
+})
+
+personModel.createPerson('apa','baa','1000')
+
+/*
+app.post('/', (request, response) => {
+  let person = personModel.()
+  response.redirect('/')
+})
+*/
 
 app.get('/messages', async (req, res) => {
   let messages = await messegaModel.getAllMessages()
   response.render("pages/messegas.ejs", {names: nameList, products: prodList})
-})
-
-app.get('/', function(request, response) {
-  response.render("pages/index.ejs", {nameList: nameList})
-})
-
-
-
-app.post('/', function(request, response) {
-  let person = personModel()
-  response.redirect('/')
 })
 
 app.listen(port, function() {
