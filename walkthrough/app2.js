554309381d6c557f8764b970037539a1578efc40
Osidur <hugo.pollack@elev.ga.ntig.se>
2020-12-01 09:51:21 +0100

    hjelp

--- walkthrough/app2.js
+++ walkthrough/app2.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const nameList = []
+
 const staticDir = __dirname + '\\client\\'
 
 app.use(express.static(staticDir))
 
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(express.static(clientDir))
 
-app.post('/', function (req, res) {
-  res.send(req.body.message)
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.send('pages/index.ejs', {name: "bbba", nameList: nameList})
+})
+
+app.post('/savePerson', function (req, res) {
+
+  let person = personModel.createPerson(req.body.name, req.body.email, req.body.age)
+
+  dBModule.storeElement(person)
+
+  res.render('pages/index.ejs', {name: req.body.name})
 })
 
 
